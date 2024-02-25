bae812932cf7091f9427b9a046ea7ba5c420703b
Muhammad Shahrom Ali <a_madsh@live.concordia.ca>
2024-02-24 22:29:44 -0500

    Move assets into separate folder - grouped by page

--- src/Screens/Screen10.js
+++ src/Screens/Screen10.js
@@ -1,4 +1,4 @@
-import "./Screen10.css";
+import "./Screen.css";
 import Window from "../components/Window";
 import { useState } from "react";
 
@@ -6,7 +6,7 @@ const Screen10 = () => {
   const [ordWindows, setOrdWindows] = useState([
     {
       layer: 4,
-      src: "./window-1@2x.png",
+      src: "./page10/window-1@2x.png",
       link: "https://y.yarn.co/7d9a564e-e131-4f7d-9ced-708018d38702_text.gif",
       dimensions: {
         top: 686,
@@ -17,7 +17,7 @@ const Screen10 = () => {
     },
     {
       layer: 3,
-      src: "./window-2@2x.png",
+      src: "./page10/window-2@2x.png",
       dimensions: {
         top: 0,
         left: 710,
@@ -27,7 +27,7 @@ const Screen10 = () => {
     },
     {
       layer: 2,
-      src: "./window-3@2x.png",
+      src: "./page10/window-3@2x.png",
       link: "https://miro.com/app/board/uXjVNyLKL2M=/",
       dimensions: {
         top: 67,
@@ -38,7 +38,7 @@ const Screen10 = () => {
     },
     {
       layer: 1,
-      src: "./window-4@2x.png",
+      src: "./page10/window-4@2x.png",
       dimensions: {
         top: 0,
         left: 960,
@@ -66,7 +66,7 @@ const Screen10 = () => {
       <img
         className="background-image"
         alt=""
-        src="/background@2x.png"
+        src="./page10/background@2x.png"
         data-scroll-to="backgroundImage"
       />
       {ordWindows.map((item, idx) => (
