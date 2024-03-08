45fa7d2194419b394cf6b188dee1893bad2000cb
Shahrom <mshahromali@gmail.com>
2024-03-08 03:14:27 -0500

    Mac Menu bar first prototype

---
+++ src/components/MacMenuBar.js
@@ -0,0 +1,37 @@
+import AppleIcon from "../icons/appleIcon";
+import ControlCenter from "../icons/controlCenter";
+import AppleSearch from "../icons/appleSearch";
+import DesktopViewer from "../icons/desktopViewer";
+
+import "./MacMenuBar.css";
+
+const MacMenuBar = (props) => {
+  return (
+    <div className="menubar-container">
+      <div className="menubar-leftside">
+        <AppleIcon />
+        <p className="menubar-program">Finder</p>
+        <p className="menubar-item">File</p>
+        <p className="menubar-item">Edit</p>
+        <p className="menubar-item">View</p>
+        <p className="menubar-item">Go</p>
+        <p className="menubar-item">Window</p>
+        <p className="menubar-item">Help</p>
+      </div>
+
+      <div className="menubar-rightside">
+        <p className="menubar-item">{"<"}</p>
+
+        <p className="menubar-item">{">"}</p>
+
+        <DesktopViewer />
+        <AppleSearch />
+        <ControlCenter />
+        <p className="menubar-item">Thu Mar 7</p>
+        <p className="menubar-item">10:09</p>
+      </div>
+    </div>
+  );
+};
+
+export default MacMenuBar;
