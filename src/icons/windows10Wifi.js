137a6f0b667a5efe20257d981649327c8f42d8a3
Shahrom <mshahromali@gmail.com>
2024-03-08 06:40:28 -0500

    Windows 10 icons

---
+++ src/icons/windows10Wifi.js
@@ -0,0 +1,20 @@
+import * as React from "react";
+const Windows10Wifi = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={19}
+    height={19}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="#fff"
+      d="M6.65 7.35c2.018-2.148 5.107-3.412 8-3.5v-1c-3.338.101-6.677 1.028-9 3.5-2.323 2.471-3.104 5.162-3 8.5h1c-.09-2.894.98-5.354 3-7.5Zm8 1.5c-3.621.11-6.112 2.379-6 6h1c-.084-2.718 2.282-4.917 5-5v-1Z"
+    />
+    <path
+      fill="#fff"
+      d="M14.65 5.85c-5.387.164-9.168 3.613-9 9h1c-.139-4.485 3.515-7.863 8-8v-1ZM13.15 14.85a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z"
+    />
+  </svg>
+);
+export default Windows10Wifi;
---
+++ src/icons/windowsSearch.js
@@ -0,0 +1,16 @@
+import * as React from "react"
+const WindowsSearch = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={17}
+    height={17}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="#000"
+      d="M10.802 0a5.844 5.844 0 0 0-4.377 9.718L.708 16.207l.794.708 5.702-6.46A5.843 5.843 0 1 0 10.802.007V0Zm0 10.625a4.781 4.781 0 1 1 0-9.562 4.781 4.781 0 0 1 0 9.562Z"
+    />
+  </svg>
+)
+export default WindowsSearch
