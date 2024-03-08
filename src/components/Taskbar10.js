22c3f94a08346befed0e423fb076e30759354c45
Shahrom <mshahromali@gmail.com>
2024-03-08 06:54:48 -0500

    Implementing Windows 10 Taskbar

---
+++ src/components/Taskbar10.js
@@ -0,0 +1,42 @@
+import "./Taskbar10.css";
+import Windows10Logo from "../icons/windows10";
+import WindowsSearch from "../icons/windowsSearch";
+import TaskView from "../icons/taskview";
+import WindowsMore from "../icons/windowsMore";
+import Windows10Charging from "../icons/windows10Charging";
+import Windows10Wifi from "../icons/windows10Wifi";
+import WindowsMute from "../icons/windowsMute";
+import Windows10Notifications from "../icons/windows10Notification";
+const Taskbar10 = (props) => {
+  return (
+    <div className="taskbar-container">
+      <div className="taskbar-leftside">
+        <Windows10Logo />
+        <div className="search-container">
+          <WindowsSearch />
+          <p className="search-text">Type here to search</p>
+        </div>
+        <TaskView />
+      </div>
+      <div className="taskbar-rightside">
+        <p className="screen-controls" onClick={props.previous}>
+          {"<"}
+        </p>
+        <p className="screen-controls" onClick={props.next}>
+          {">"}
+        </p>
+
+        <WindowsMore />
+        <Windows10Charging />
+        <Windows10Wifi />
+        <WindowsMute />
+        <p className="date-time">ENG</p>
+        <p className="date-time">{"10:09"}</p>
+        <p className="date-time">01/12/2022</p>
+        <Windows10Notifications />
+      </div>
+    </div>
+  );
+};
+
+export default Taskbar10;
