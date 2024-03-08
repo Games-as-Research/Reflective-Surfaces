5d1db348b7d2b4447466a9bb2ede334492852998
Shahrom <mshahromali@gmail.com>
2024-03-08 05:22:28 -0500

    Adding Vadim's Windows

--- src/Screens/Screen8.js
+++ src/Screens/Screen8.js
@@ -4,33 +4,63 @@ import { useState } from "react";
 const Screen8 = (props) => {
   const [ordWindows, setOrdWindows] = useState([
     {
-      layer: 3,
-      src: "./S3_Rilla/Abstract/abstract.png",
+      layer: 6,
+      src: "./S8_Vadim/word1.png",
       dimensions: {
-        top: 173,
-        left: 81,
-        width: 981,
-        height: 325,
+        top: 82,
+        left: 891,
+        width: 658,
+        height: 759,
+      },
+    },
+    {
+      layer: 5,
+      src: "./S8_Vadim/word2.png",
+      dimensions: {
+        top: 280,
+        left: 624,
+        width: 596,
+        height: 686,
+      },
+    },
+    {
+      layer: 4,
+      src: "./S8_Vadim/unity.png",
+      dimensions: {
+        top: 606,
+        left: 326,
+        width: 521,
+        height: 361,
+      },
+    },
+    {
+      layer: 3,
+      src: "./S8_Vadim/trello.png",
+      dimensions: {
+        top: 82,
+        left: 326,
+        width: 476,
+        height: 598,
       },
     },
     {
       layer: 2,
-      src: "./S3_Rilla/Abstract/docs.png",
+      src: "./S8_Vadim/gitKraken.png",
       dimensions: {
-        top: 193,
-        left: 436,
-        width: 1047,
-        height: 936,
+        top: 216,
+        left: 87,
+        width: 492,
+        height: 304,
       },
     },
     {
       layer: 1,
-      src: "./S3_Rilla/Abstract/pdf.png",
+      src: "./S8_Vadim/ryder.png",
       dimensions: {
-        top: 48,
-        left: 69,
-        width: 1592,
-        height: 1066,
+        top: 606,
+        left: 86,
+        width: 490,
+        height: 290,
       },
     },
   ]);
