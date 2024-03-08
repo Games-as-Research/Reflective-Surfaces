02fdb373e86a84db8ac719a36f05235561fdddad
Shahrom <mshahromali@gmail.com>
2024-03-08 04:13:36 -0500

    Adding rest of the screens
    Dont include their windows

--- src/Screens.js
+++ src/Screens.js
@@ -1,14 +1,53 @@
 import { useState } from "react";
-import Screen10 from "./Screens/Screen10";
-import Screen01 from "./Screens/Screen01";
+
+import Screen1 from "./Screens/Screen1";
+import Screen2 from "./Screens/Screen2";
+import Screen3 from "./Screens/Screen3";
+import Screen4 from "./Screens/Screen4";
+import Screen5 from "./Screens/Screen5";
+import Screen6 from "./Screens/Screen6";
+import Screen7 from "./Screens/Screen7";
+import Screen8 from "./Screens/Screen8";
+import Screen9 from "./Screens/Screen9";
 
 const Screens = (props) => {
   const [activeScreen, setActiveScreen] = useState(1);
 
+  function nextScreen() {
+    console.log("Next Screen");
+    if (activeScreen === 9) {
+      setActiveScreen(1);
+    } else {
+      setActiveScreen(activeScreen + 1);
+    }
+  }
+
+  function previousScreen() {
+    if (activeScreen === 1) {
+      setActiveScreen(9);
+    } else {
+      setActiveScreen(activeScreen - 1);
+    }
+  }
+
   if (activeScreen === 1) {
-    return <Screen01 />;
-  } else if (activeScreen === 10) {
-    return <Screen10 />;
+    return <Screen1 nextScreen={nextScreen} previousScreen={previousScreen} />;
+  } else if (activeScreen === 2) {
+    return <Screen2 nextScreen={nextScreen} previousScreen={previousScreen} />;
+  } else if (activeScreen === 3) {
+    return <Screen3 nextScreen={nextScreen} previousScreen={previousScreen} />;
+  } else if (activeScreen === 4) {
+    return <Screen4 nextScreen={nextScreen} previousScreen={previousScreen} />;
+  } else if (activeScreen === 5) {
+    return <Screen5 nextScreen={nextScreen} previousScreen={previousScreen} />;
+  } else if (activeScreen === 6) {
+    return <Screen6 nextScreen={nextScreen} previousScreen={previousScreen} />;
+  } else if (activeScreen === 7) {
+    return <Screen7 nextScreen={nextScreen} previousScreen={previousScreen} />;
+  } else if (activeScreen === 8) {
+    return <Screen8 nextScreen={nextScreen} previousScreen={previousScreen} />;
+  } else if (activeScreen === 9) {
+    return <Screen9 nextScreen={nextScreen} previousScreen={previousScreen} />;
   } else {
     return <></>;
   }
---
+++ src/Screens/Screen6.js
@@ -0,0 +1,49 @@
+import Screen from "../components/Screen";
+import { useState } from "react";
+
+const Screen6 = (props) => {
+  const [ordWindows, setOrdWindows] = useState([
+    {
+      layer: 3,
+      src: "./S3_Rilla/Abstract/abstract.png",
+      dimensions: {
+        top: 173,
+        left: 81,
+        width: 981,
+        height: 325,
+      },
+    },
+    {
+      layer: 2,
+      src: "./S3_Rilla/Abstract/docs.png",
+      dimensions: {
+        top: 193,
+        left: 436,
+        width: 1047,
+        height: 936,
+      },
+    },
+    {
+      layer: 1,
+      src: "./S3_Rilla/Abstract/pdf.png",
+      dimensions: {
+        top: 48,
+        left: 69,
+        width: 1592,
+        height: 1066,
+      },
+    },
+  ]);
+
+  return (
+    <Screen
+      windows={ordWindows}
+      setWindows={setOrdWindows}
+      backgroundImage={"./S6_Enric/Background.jpg"}
+      nextScreen={props.nextScreen}
+      previousScreen={props.previousScreen}
+    />
+  );
+};
+
+export default Screen6;
