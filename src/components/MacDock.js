efd10b420a5f5263237fcf4cbcce26154236a149
Muhammad Shahrom Ali <a_madsh@live.concordia.ca>
2024-05-06 00:13:58 -0400

    more dock icons

--- src/components/MacDock.js
+++ src/components/MacDock.js
@@ -4,13 +4,19 @@ const MacDock = (props) => {
   return (
     <div className="dock-container">
       <DockIcon src={"./icons/finder.png"} />
-      
-      
-      
+
       {/* Add the rest of icons here */}
-      
-      
-      
+
+      <DockIcon src={"./icons/chrome.png"} />
+      <DockIcon src={"./icons/vscode.png"} />
+      <DockIcon src={"./icons/preview.png"} />
+      <DockIcon src={"./icons/garage_band.png"} />
+      <DockIcon src={"./icons/textedit.png"} />
+      <DockIcon src={"./icons/discord.png"} />
+      <DockIcon src={"./icons/obsidian.png"} />
+      <DockIcon src={"./icons/firefox.png"} />
+      <DockIcon src={"./icons/rider.png"} />
+
       <div className="divider" />
       {props.trashEmpty ? (
         <DockIcon src={"./icons/Trash-Empty-icon.png"} />
