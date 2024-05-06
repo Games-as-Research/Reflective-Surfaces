323a99cf620a722529307fdb4238f0209cb41cbc
Muhammad Shahrom Ali <a_madsh@live.concordia.ca>
2024-05-05 23:43:22 -0400

    Completed Mac Dock

--- src/components/MacDock.js
+++ src/components/MacDock.js
@@ -1,7 +1,28 @@
 import "../stylesheets/MacDock.css";
 
 const MacDock = (props) => {
-  return <div className="dock-container"></div>;
+  return (
+    <div className="dock-container">
+      <DockIcon src={"./icons/finder.png"} />
+      
+      
+      
+      {/* Add the rest of icons here */}
+      
+      
+      
+      <div className="divider" />
+      {props.trashEmpty ? (
+        <DockIcon src={"./icons/Trash-Empty-icon.png"} />
+      ) : (
+        <DockIcon src={"./icons/Trash-full-icon.png"} />
+      )}
+    </div>
+  );
+};
+
+const DockIcon = (props) => {
+  return <img className="icon" alt="dock icon" src={props.src} />;
 };
 
 export default MacDock;
