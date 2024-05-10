54b7fa69335d2cb77186cce77e7cf3d825712af0
Muhammad Shahrom Ali <a_madsh@live.concordia.ca>
2024-05-10 04:51:02 -0400

    Adding the rest of Rilla's Screens

--- src/all-screens/screen3.js
+++ src/all-screens/screen3.js
@@ -6,8 +6,20 @@ const DEFAULT_STATE = {
   background: "./S3_Rilla/Background.jpeg",
   windows: [
     {
-      layer: 3,
-      minimized: false,
+      layer: 4,
+      minimized: true,
+      type: "FINDER",
+      src: "./S3_Rilla/Closing/file_explorer.png",
+      dimensions: {
+        top: 96,
+        left: 55,
+        width: 942,
+        height: 227,
+      },
+    },
+    {
+      layer: 9,
+      minimized: true,
       type: "TEXTEDIT",
       src: "./S3_Rilla/Abstract/abstract.png",
       dimensions: {
@@ -18,8 +30,8 @@ const DEFAULT_STATE = {
       },
     },
     {
-      layer: 2,
-      minimized: false,
+      layer: 8,
+      minimized: true,
       type: "CHROME",
       src: "./S3_Rilla/Abstract/docs.png",
       dimensions: {
@@ -30,19 +42,81 @@ const DEFAULT_STATE = {
       },
     },
     {
-      layer: 1,
-      minimized: false,
+      layer: 7,
+      minimized: true,
       type: "PREVIEW",
       src: "./S3_Rilla/Abstract/pdf.png",
       dimensions: {
         top: 129,
         left: 384,
-        width: 1260,
-        height: 833,
+        width: 780,
+        height: 516,
+      },
+    },
+    // P2
+    {
+      layer: 6,
+      minimized: true,
+      type: "DISCORD",
+      src: "./S3_Rilla/Closing/discordJoey.png",
+      dimensions: {
+        top: 129,
+        left: 384,
+        width: 713,
+        height: 613,
+      },
+    },
+    {
+      layer: 5,
+      minimized: true,
+      type: "CHROME",
+      src: "./S3_Rilla/Closing/docs_rTd.png",
+      dimensions: {
+        top: 96,
+        left: 55,
+        width: 857,
+        height: 916,
+      },
+    },
+
+    {
+      layer: 4,
+      minimized: true,
+      type: "DISCORD",
+      src: "./S3_Rilla/Context/discord_matt.png",
+      dimensions: {
+        top: 96,
+        left: 55,
+        width: 857,
+        height: 916,
+      },
+    },
+    {
+      layer: 3,
+      minimized: true,
+      type: "CHROME",
+      src: "./S3_Rilla/Context/docs_context.png",
+      dimensions: {
+        top: 96,
+        left: 55,
+        width: 857,
+        height: 916,
+      },
+    },
+    {
+      layer: 2,
+      minimized: true,
+      type: "GITHUB",
+      src: "./S3_Rilla/Context/gh_desktop.png",
+      dimensions: {
+        top: 96,
+        left: 55,
+        width: 555,
+        height: 361,
       },
     },
   ],
-  dock_icons: ["DISCORD", "GITHUB"],
+  dock_icons: [],
 };
 
 const Screen3 = (props) => {
@@ -51,7 +125,7 @@ const Screen3 = (props) => {
   useEffect(() => {
     GameMan.unlockScreen(2);
   }, []);
-  
+
   return <Screen state={DEFAULT_STATE} />;
 };
 
