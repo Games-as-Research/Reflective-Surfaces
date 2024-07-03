0deac9125ce8b1a4c6ab35545b75d2fd8a940af1
Shahrom <mshahromali@gmail.com>
2024-07-03 04:44:59 -0400

    Vadim's screens

--- src/all-screens/screen8.js
+++ src/all-screens/screen8.js
@@ -19,7 +19,7 @@ const DEFAULT_STATE = {
         height: 759,
       },
       map: {
-        name: "Screen8Window1",
+        name: "Screen8Window6",
         areas: [
           {
             id: "824v520v-d82n-81fv-0d82-bd24f3fb79f4",
@@ -62,6 +62,27 @@ const DEFAULT_STATE = {
         width: 521,
         height: 361,
       },
+      map: {
+        name: "Screen8Window4",
+        areas: [
+          {
+            id: "8240850v-d8fN-81fv-0d82-bd24f3fb79f4",
+            title: "unity-play--hyperlink",
+            url: "https://zolarsystems.itch.io/soundgarden",
+            shape: "poly",
+            name: "3",
+            fillColor: "#84BDE388",
+            strokeColor: "black",
+            coords: [700, 70, 830, 70, 830, 100, 700, 100],
+            polygon: [
+              [700, 70],
+              [830, 70],
+              [830, 100],
+              [700, 100],
+            ],
+          },
+        ],
+      },
     },
     {
       layer: 3,
@@ -75,7 +96,7 @@ const DEFAULT_STATE = {
         height: 598,
       },
       map: {
-        name: "Screen8Window2",
+        name: "Screen8Window3",
         areas: [
           {
             id: "824v520v-d82N-81fv-0d82-bd24f3fb79f4",
@@ -92,6 +113,39 @@ const DEFAULT_STATE = {
               [950, 750],
             ],
           },
+          {
+            id: "824vff0v-d8fN-81fv-0d82-bd24f3fb79f4",
+            title: "trello-link--hyperlink",
+            url: "https://trello.com/c/vgg4r0Tq",
+            shape: "poly",
+            name: "2",
+            fillColor: "#84BDE388",
+            strokeColor: "black",
+            coords: [210, 160, 1185, 160, 1185, 300, 210, 300],
+            polygon: [
+              [210, 160],
+              [1185, 160],
+              [1185, 300],
+              [210, 300],
+            ],
+          },
+
+          {
+            id: "824iff0v-d8fN-81fv-0d82-bd24f3fb79f4",
+            title: "trello-link--hyperlink",
+            url: "https://trello.com/b/2r2i7bm1/zolar-public",
+            shape: "poly",
+            name: "3",
+            fillColor: "#84BDE388",
+            strokeColor: "black",
+            coords: [180, 50, 780, 50, 780, 100, 180, 100],
+            polygon: [
+              [180, 50],
+              [780, 50],
+              [780, 100],
+              [180, 100],
+            ],
+          },
         ],
       },
     },
@@ -105,6 +159,43 @@ const DEFAULT_STATE = {
         left: 87,
         width: 492,
         height: 304,
+      },
+      map: {
+        name: "Screen8Window2",
+        areas: [
+          {
+            id: "824080v-d8fN-81fv-0d82-bd2fb79f4",
+            title: "githubpages--hyperlink",
+            url: "https://videon.github.io/soundgarden/",
+            shape: "poly",
+            name: "1",
+            fillColor: "#84BDE388",
+            strokeColor: "black",
+            coords: [425, 65, 610, 65, 610, 105, 425, 105],
+            polygon: [
+              [425, 65],
+              [610, 65],
+              [610, 105],
+              [425, 105],
+            ],
+          },
+          {
+            id: "824080v-d8fN-81fv-0d82-bi2cb79f4",
+            title: "github-profile--hyperlink",
+            url: "https://github.com/Videon",
+            shape: "poly",
+            name: "1",
+            fillColor: "#84BDE388",
+            strokeColor: "black",
+            coords: [930, 400, 1190, 400, 1190, 480, 930, 480],
+            polygon: [
+              [930, 400],
+              [1190, 65],
+              [1190, 480],
+              [930, 480],
+            ],
+          },
+        ],
       },
     },
     {
@@ -134,7 +225,7 @@ const Screen8 = (props) => {
   return (
     <>
       {/* <P5JS /> */}
-      <Screen state={DEFAULT_STATE} backgroundColor={"#7A5C10"}/>
+      <Screen state={DEFAULT_STATE} backgroundColor={"#7A5C10"} />
     </>
   );
 };
